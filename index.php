6c77e245e75f29f39410a9a364694f3a75942a20
Pier-Luc Brault <pier-luc@brault.me>
2025-03-17 22:00:56 -0400

    Correction du title de la page d'accueil

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $pages = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laboratoire 03</title>
+    <title>Laboratoire 03-A</title>
     <link rel="stylesheet" href="water.css">
 </head>
 <body>
